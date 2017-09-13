e045a18e77dcb81aa8de2c9803a6141fa1748628
jack33232 <jack33232@gmail.com>
2017-09-13 12:33:52 +0800

    - Add function to do the update on duplicate

--- Query/Builder.php
+++ Query/Builder.php
@@ -2172,6 +2172,50 @@ class Builder
         return (bool) $this->where($attributes)->take(1)->update($values);
     }
 
+    public function updateOnDuplicate(array $values, array $fields = [])
+    {
+        if (empty($values)) {
+            return true;
+        }
+
+        // Since every insert gets treated like a batch insert, we will make sure the
+        // bindings are structured in a way that is convenient for building these
+        // inserts statements by verifying the elements are actually an array.
+        if (! is_array(reset($values))) {
+            $values = [$values];
+        }
+
+        // Since every insert gets treated like a batch insert, we will make sure the
+        // bindings are structured in a way that is convenient for building these
+        // inserts statements by verifying the elements are actually an array.
+        else {
+            foreach ($values as $key => $value) {
+                ksort($value);
+                $values[$key] = $value;
+            }
+        }
+
+        // We'll treat every insert like a batch insert so we can easily insert each
+        // of the records into the database consistently. This will make it much
+        // easier on the grammars to just handle one type of record insertion.
+        $bindings = [];
+
+        foreach ($values as $record) {
+            foreach ($record as $value) {
+                $bindings[] = $value;
+            }
+        }
+
+        $sql = $this->grammar->compileUpdateOnDuplicate($this, $values, $fields);
+
+        // Once we have compiled the insert statement's SQL we can execute it on the
+        // connection and return a result as a boolean success indicator as that
+        // is the same type of result returned by the raw connection instance.
+        $bindings = $this->cleanBindings($bindings);
+
+        return $this->connection->insert($sql, $bindings);
+    }
+
     /**
      * Increment a column's value by a given amount.
      *
--- Query/Grammars/MySqlGrammar.php
+++ Query/Grammars/MySqlGrammar.php
@@ -139,6 +139,43 @@ class MySqlGrammar extends Grammar
         return rtrim($sql);
     }
 
+    public function compileUpdateOnDuplicate(Builder $query, array $values, array $fields)
+    {
+        // Essentially we will force every insert to be treated as a batch insert which
+        // simply makes creating the SQL easier for us since we can utilize the same
+        // basic routine regardless of an amount of records given to us to insert.
+        $table = $this->wrapTable($query->from);
+
+        if (! is_array(reset($values))) {
+            $values = [$values];
+        }
+
+        $columns = $this->columnize(array_keys(reset($values)));
+
+        // We need to build a list of parameter place-holders of values that are bound
+        // to the query. Each insert should have the exact same amount of parameter
+        // bindings so we will loop through the record and parameterize them all.
+        $parameters = [];
+
+        foreach ($values as $record) {
+            $parameters[] = '('.$this->parameterize($record).')';
+        }
+
+        $parameters = implode(', ', $parameters);
+
+        $update_fields = [];
+
+        foreach ($fields as $field) {
+            $field = $this->wrap($field);
+            $update_fields[] = $field . ' =  VALUES(' . $field . ')';
+        }
+
+        $update_fields = implode(', ', $update_fields);
+
+        return "insert into $table ($columns) values $parameters ON 
+DUPLICATE KEY UPDATE $update_fields";
+    }
+
     /**
      * Prepares a JSON column being updated using the JSON_SET function.
      *
